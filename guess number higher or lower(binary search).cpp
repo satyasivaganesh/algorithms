98b269a8f6cf3ec8c79e8cc21023cf12978811a3
satyasivaganesh <84192998+satyasivaganesh@users.noreply.github.com>
2022-03-20 23:10:03 +0530

    Create guess number higher or lower(binary search).cpp

---
+++ guess number higher or lower(binary search).cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int guessNumber(int n) {
+        int l=1;
+        int h=n;
+        int m;
+        while(l<=h)
+        {
+            m=l+(h-l)/2;
+            if(guess(m)==-1)
+                h=m-1;
+            else if(guess(m)==1)
+                l=m+1;
+            else
+                return m;
+        }
+        return h;
+        
+    }
+};
